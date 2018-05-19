0668a5415c0c2eeb5e7860106923b4dd4d8db71e
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 11:42:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on("message", async message => {
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
-  if(!commandfile.commands) return message.channel.send("Command Not Found");
+  if(!commandfile) return message.channel.send("Command Not Found");
 });
 
 bot.login(process.env.botToken).catch(err => console.log(err));
