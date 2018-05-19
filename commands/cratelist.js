3105a0439141deddf4f3f12d04452acdd7b901e8
TheLavaPhoenix <ilyes05tata@gmail.com>
2018-05-19 10:07:21 -0700

    Update cratelist.js

--- commands/cratelist.js
+++ commands/cratelist.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let cratelist = new DIscord.RichEmbed()
+let cratelist = new Discord.RichEmbed()
 .addField("⚪Common Crate⚪" , "25$");
 message.channel.send(cratelist)
 }
