4ec96e222addd0ef57f5cac7af98f81d3a43c5ea
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 11:39:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,14 +38,16 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
+  
   let prefix = botconfig.prefix;
-  if(!message.content.startsWith(prefix)) return
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
+  
+  return message.channel.send("Command Not Found");
 });
 
 bot.login(process.env.botToken).catch(err => console.log(err));
