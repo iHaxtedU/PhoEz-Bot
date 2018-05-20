bdbea28aa41c3234da9094fbf16d1370b06fdcb1
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 20:27:52 -0500

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+let who = message.mentions.first();
+if (!who) return message.channel.send("User Not Found, Or Specified");
+let roletogive = args[1];
+if (!roletogive) return message.channel.send("Please Include The Role To Give After User");
+let role = bot.roles.find('name', roletogive);
+if (!role) return message.channel.send("Role Not Found");
+  
+who.giveRole(role);
+message.channel.send(`Successfully Gave ${who} ${role}!`);
+
+}
+
+module.exports.help = {
+  name: "giverole"
+}
