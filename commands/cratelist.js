c607b58e06ddd5462a580bb470c1c2e7d302a9ac
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 20:05:58 -0500

    Update cratelist.js

--- commands/cratelist.js
+++ commands/cratelist.js
@@ -2,8 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 let cratelist = new Discord.RichEmbed()
-.addField("⚪Common Crate⚪" , "25$");
-.addField("⚪Common Crate⚪" , "25$" , true);
+.setAuthor(bot.user.username, bot.user.avatarURL)
+.setColor("#ff2dc3")
+.addField(":ballot_box: Common Crate :ballot_box:", "$25");
+
 message.channel.send(cratelist)
 }
 
