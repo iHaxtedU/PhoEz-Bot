277bdda02642b0ee1fb19bdcc0b1e47b78bf986e
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 20:53:59 -0500

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -5,10 +5,10 @@ let who = message.mentions.users.first();
 if (!who) return message.channel.send("User Not Found, Or Specified");
 let roletogive = args[1];
 if (!roletogive) return message.channel.send("Please Include The Role To Give After User");
-let role = bot.roles.find('name', roletogive);
+let role = message.guild.roles.find('name," roletogive)
 if (!role) return message.channel.send("Role Not Found");
   
-who.giveRole(role);
+who.addRole(role);
 message.channel.send(`Successfully Gave ${who} ${role}!`);
 
 }
