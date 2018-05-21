c585a6f1628d9bc817ec23588ae67ceb68da3c7b
TheLavaPhoenix <ilyes05tata@gmail.com>
2018-05-20 19:04:07 -0700

    Update commoncrate.js

--- commands/commoncrate.js
+++ commands/commoncrate.js
@@ -1,8 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+let money = JSON.parse(fs.readFileSync("../money.json" , "utf8"));
+let wUser = message.author;
+let moneylevel = money[wUser.id] ? money[wUser.id].money : 0;
 
-message.channel.send("You successfully bought a common crate!")
+if(moneylevel < 25){
+
+message.channel.send("You successfully did not buy a common crate!")
+message.channel.send(`You need ${25 - moneylevel}$ to buy this!`)
+  }
 }
 
 module.exports.help = {
